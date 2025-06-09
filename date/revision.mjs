d711c80b5f6c3393868d5207a2b85bd82271c067
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2025-06-09 20:54:43 +0100

    create getRevisionDates function

--- date/revision.mjs
+++ date/revision.mjs
@@ -1,3 +1,6 @@
 // Exports getRevisionDates(startDate)
 // Generates spaced repetition dates (1 week, 1 month, etc.)
 // Returns revision dates as an array of objects
+export function getRevisionDates(){
+    
+}
