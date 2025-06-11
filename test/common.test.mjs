b3bf9d61f7e65a7d27ecdd834767d460a7e69462
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2025-06-11 22:20:28 +0100

    test1

--- test/common.test.mjs
+++ test/common.test.mjs
@@ -1,7 +1,20 @@
 import { getUserIDs } from "../data/common.mjs";
+import { getRevisionDates } from '../date/utils.mjs';
 import assert from "node:assert";
 import test from "node:test";
 
 test("User count is correct", () => {
-  assert.equal(getUserIDs().length, 5);
+  assert.equal(getUserIDs().length, 5); // update 5 to your actual expected count
+});
+
+test('calculate correct repetition dates', () => {
+  const dates = getRevisionDates('2025-07-19', 'Functions');
+  const expected = [
+    { topic: 'Functions', date: '2025-07-26' },
+    { topic: 'Functions', date: '2025-08-18' },
+    { topic: 'Functions', date: '2025-10-17' },
+    { topic: 'Functions', date: '2026-01-15' },
+    { topic: 'Functions', date: '2026-07-19' }
+  ];
+  assert.deepEqual(dates, expected);
 });
